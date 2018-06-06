89f6e0b8c4238328ca43323c48e9c28cc0feeec9
David Merfield <dmerfield@gmail.com>
2018-06-06 17:51:55 -0400

    Work on sync feature

--- test/sync.js
+++ test/sync.js
@@ -4,6 +4,85 @@ var resetDropboxFolder = require('./resetDropboxFolder');
 var resetDataFolder = require('./resetDataFolder');
 var fs = require('fs-extra');
 var timeout = require('./timeout')(jasmine, 60*1000*5); // 5 min
+var join = require('path').join;
+var dataFolder = __dirname + '/data';
+var async = require('async');
+
+function arraysEqual (arr1, arr2) {
+
+    // sort the arrays
+    arr1.sort();
+    arr2.sort();
+
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+
+    return true;
+}
+
+var debug = require('debug')('sync');
+
+function checkReaddirInFolders (dbRoot, fsRoot, callback) {
+
+  debug('db reading', dbRoot);
+
+  dropbox.readdir(dbRoot, function(dbErr, dbContents){
+
+    debug('fs reading', dbRoot);
+  
+    fs.readdir(fsRoot, function(fsErr, fsContents){
+
+      debug('fs err', fsErr);
+      debug('db err', dbErr);
+
+      debug('fs Contents', fsContents);
+      debug('db Contents', dbContents);
+      
+      if (fsErr || dbErr) return callback(fsErr || dbErr);
+
+      if (!arraysEqual(dbContents, fsContents)) {
+        return callback(new Error('Contents are not equal'));
+      }
+
+      async.eachOf(dbContents, function(name, index, next){
+
+        fs.stat(join(fsRoot, name), function(fsErr, fsStat){
+
+          dropbox.stat(join(dbRoot, name), function(dbErr, dbStat){
+
+            if (fsErr || dbErr) return callback(fsErr || dbErr);
+
+            if (fsStat.isDirectory() !== dbStat.isDirectory()) {
+              return callback(new Error(join(fsRoot, name) + ' isDirectory does not match'));
+            }
+            
+            if (fsStat.isFile() !== dbStat.isFile()) {
+              return callback(new Error(join(fsRoot, name) + ' isFile does not match'));
+            }
+
+            if (dbStat.isDirectory()) {
+              return checkReaddirInFolders(join(dbRoot, name), join(fsRoot, name), next);              
+            }
+
+            if (fsStat.size !== dbStat.size) {
+              return callback(new Error(join(fsRoot, name) + ' Size does not match'));
+            }
+
+            if (fsStat.mtime.valueOf() !== dbStat.mtime.valueOf()) {
+              return callback(new Error(join(fsRoot, name) + ' mtime does not match'));
+            }
+
+            next(null);
+          });
+        });
+      }, callback);
+    });
+  });
+}
 
 describe("sync", function() {
 
@@ -14,9 +93,39 @@ describe("sync", function() {
   beforeEach(timeout.extend);
   afterEach(timeout.reset);
 
-  xit("syncs a folder", function(done){
+  it("syncs a file", function(done){
 
-    var dataFolder = __dirname + '/data';
+    dropbox.writeFile('/foo/bar/baz.txt', 'Hello', function(err){
+
+      expect(err).toBe(null);
+
+      dropbox.sync('/', dataFolder, function(err){
+
+        expect(err).toBe(null);
+
+        checkReaddirInFolders('/', dataFolder, function(err){
+
+          expect(err).toBe(null);
+          done();
+        });
+      });
+    });
+
+  });
+
+  /*
+  
+  need to test for reset err
+
+error:
+      { [Error: cannot POST /2/files/list_folder/continue (409)]
+        status: 409,
+        text: '{"error_summary": "reset/", "error": {".tag": "reset"}}',
+
+  */ 
+
+
+  it("syncs a folder", function(done){
 
     var localFileWhichShouldLeavePath = dataFolder + '/foo/bar.txt';
     var localFileWhichShouldLeaveContents = 'Hey there';
@@ -36,17 +145,11 @@ describe("sync", function() {
 
           expect(err).toBe(null);
           
-          dropbox.readdir('/', function(err, dropboxItems){
+          checkReaddirInFolders('/', dataFolder, function(err){
 
             expect(err).toBe(null);
-
-            fs.readdirSync(dataFolder, function(err, localItems){
-
-              expect(err).toBe(null);
-              expect(localItems.sort()).toEqual(dropboxItems.sort);
-              done();
-            });
-          });          
+            done();
+          });       
         });
       });
     });
