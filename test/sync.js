0e7c6dec55666d2c7f7ca47c4ed831c6140806d6
David Merfield <dmerfield@gmail.com>
2018-06-08 17:46:14 -0400

    Improvements to sync method

--- test/sync.js
+++ test/sync.js
@@ -24,188 +24,241 @@ describe("sync", function() {
 
   // Ensure the test account's Dropbox folder
   // is empty before running each test.
-  beforeEach(resetDropboxFolder);
-  beforeEach(resetDataFolder);
-  beforeEach(timeout.extend);
-  afterEach(timeout.reset);
-  beforeEach(console.log);
-  afterEach(console.log);
 
-  xit("removes a local folder which does not exist on dropbox", function(done){
-
-    fs.ensureDirSync(dataFolder + '/foo');
-
-    dropbox.sync('/', dataFolder, function(err){
-
-      expect(err).toBe(null);
-
-      checkSync('/', dataFolder, function(err){
-
-        expect(err).toBe(null);
-        done();
-      });       
-    });
-  });
-
-
-  xit("removes a local file which does not exist on dropbox", function(done){
-
-    fs.outputFile(dataFolder + '/foo.txt', 'Hello');
-
-    dropbox.sync('/', dataFolder, function(err){
-
-      expect(err).toBe(null);
-
-      checkSync('/', dataFolder, function(err){
-
-        expect(err).toBe(null);
-        done();
-      });       
-    });
-  });
-
-
-  xit("downloads a file from Dropbox", function(done){
-
-    dropbox.writeFile('/baz.txt', 'Hello', function(err){
-
-      expect(err).toBe(null);
-
-      dropbox.sync('/', dataFolder, function(err){
-
-        expect(err).toBe(null);
-
-        checkSync('/', dataFolder, function(err){
-
-          expect(err).toBe(null);
-          done();
-        });
-      });
-    });
-  });
-
-  xit("makes a folder which is on from Dropbox", function(done){
-
-    dropbox.mkdir('/hey', function(err){
-
-      expect(err).toBe(null);
-
-      dropbox.sync('/', dataFolder, function(err){
-
-        expect(err).toBe(null);
-
-        checkSync('/', dataFolder, function(err){
-
-          expect(err).toBe(null);
-          done();
-        });
-      });
-    });
-  });
-
-
-  xit("updates a file which exists but is stale", function(done){
-
-    fs.outputFileSync(dataFolder + '/test.txt', 'Hey');
-
-    dropbox.writeFile('/test.txt', 'No', function(err){
-
-      expect(err).toBe(null);
-
-      dropbox.sync('/', dataFolder, function(err){
-
-        expect(err).toBe(null);
-
-        checkSync('/', dataFolder, function(err){
-
-          expect(err).toBe(null);
-          done();
-        });
-      });
-    });
-  });
-
-  xit("replaces a folder with a file", function(done){
-
-    fs.ensureDirSync(dataFolder + '/test.txt');
-
-    dropbox.writeFile('/test.txt', 'No', function(err){
-
-      expect(err).toBe(null);
-
-      dropbox.sync('/', dataFolder, function(err){
-
-        expect(err).toBe(null);
-
-        checkSync('/', dataFolder, function(err){
-
-          expect(err).toBe(null);
-          done();
-        });
-      });
-    });
-  });
+  describe("in root of Dropbox", main('/'));
+  describe("in a subfolder of Dropbox", main('/a/b/c'));
   
-  it("will not redownload a file which matches", function(done){
+  function main (dropboxFolder) { return function () {
 
-    fs.writeFileSync(dataFolder + '/rabbit.txt', 'hare');
-  
-    dropbox = lib(accessToken);
-    spyOn(dropbox, 'download');
+    beforeEach(resetDropboxFolder);
+    beforeEach(resetDataFolder);
+      
+    // if running the tests inside a subdirectory, ensure it exists
+    beforeEach(function(done){
 
-    dropbox.writeFile('/rabbit.txt', 'hare', function(err){
+      if (dropboxFolder === '/') return done();
+      
+      dropbox.emptyDir(dropboxFolder, done);
+    });
 
-      expect(err).toBe(null);
+    beforeEach(timeout.extend);
+    afterEach(timeout.reset);
+    beforeEach(console.log);
+    afterEach(console.log);
 
-      dropbox.sync('/', dataFolder, function(err){
+    it("removes a local folder which does not exist on dropbox", function(done){
+
+      fs.ensureDirSync(dataFolder + '/foo');
+
+      dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
 
         expect(err).toBe(null);
+        expect(cursor).toEqual(jasmine.any(String));
+        expect(additions).toEqual([]);
+        expect(deletions).toEqual(['/foo']);
 
-        console.log('Checking sync!');
-
-        checkSync('/', dataFolder, function(err){
-
-          console.log('Back here sync!');
+        checkSync(dropboxFolder, dataFolder, function(err){
 
           expect(err).toBe(null);
-          expect(dropbox.download).toHaveBeenCalled();
-
-          console.log('Calling done!');
           done();
-        });
+        });       
       });
     });
-  });
 
 
-  xit("syncs a folder", function(done){
+    it("removes a local file which does not exist on dropbox", function(done){
 
-    var localFileWhichShouldLeavePath = dataFolder + '/foo/bar.txt';
-    var localFileWhichShouldLeaveContents = 'Hey there';
+      fs.outputFile(dataFolder + '/foo.txt', 'Hello');
 
-    var dropboxFilePath = '/baz/bar.txt';
-    var dropboxFileContents = 'Hello';
+      dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
 
-    fs.outputFile(localFileWhichShouldLeavePath, localFileWhichShouldLeaveContents, function(err){
+        expect(err).toBe(null);
+        expect(cursor).toEqual(jasmine.any(String));
+        expect(additions).toEqual([]);
+        expect(deletions).toEqual(['/foo.txt']);
 
-      expect(err).toBe(null);
+        checkSync(dropboxFolder, dataFolder, function(err){
 
-      dropbox.writeFile(dropboxFilePath, dropboxFileContents, function(err){
+          expect(err).toBe(null);
+          done();
+        });       
+      });
+    });
+
+
+    it("downloads a file from Dropbox", function(done){
+
+      dropbox.writeFile(join(dropboxFolder, 'baz.txt'), 'Hello', function(err){
 
         expect(err).toBe(null);
 
-        dropbox.sync('/', dataFolder, function(err){
+        dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
 
           expect(err).toBe(null);
-          
-          checkSync('/', dataFolder, function(err){
+          expect(cursor).toEqual(jasmine.any(String));
+          expect(additions).toEqual(['/baz.txt']);
+          expect(deletions).toEqual([]);
+
+          checkSync(dropboxFolder, dataFolder, function(err){
 
             expect(err).toBe(null);
             done();
-          });       
+          });
         });
       });
     });
-  });
+
+    it("makes a folder which is on from Dropbox", function(done){
+
+      dropbox.mkdir(join(dropboxFolder, 'hey'), function(err){
+
+        expect(err).toBe(null);
+
+        dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
+
+          expect(err).toBe(null);
+          expect(cursor).toEqual(jasmine.any(String));
+          expect(additions).toEqual(['/hey']);
+          expect(deletions).toEqual([]);
+
+          checkSync(dropboxFolder, dataFolder, function(err){
+
+            expect(err).toBe(null);
+            done();
+          });
+        });
+      });
+    });
+
+
+    it("updates a file which exists but is stale", function(done){
+
+      fs.outputFileSync(dataFolder + '/test.txt', 'Hey');
+
+      dropbox.writeFile(join(dropboxFolder, 'test.txt'), 'No', function(err){
+
+        expect(err).toBe(null);
+
+        dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
+
+          expect(err).toBe(null);
+          expect(cursor).toEqual(jasmine.any(String));
+          expect(additions).toEqual(['/test.txt']);
+          expect(deletions).toEqual([]);
+        
+          checkSync(dropboxFolder, dataFolder, function(err){
+
+            expect(err).toBe(null);
+            done();
+          });
+        });
+      });
+    });
+
+    it("replaces a folder with a file", function(done){
+
+      fs.ensureDirSync(dataFolder + '/test.txt');
+
+      dropbox.writeFile(join(dropboxFolder, 'test.txt'), 'No', function(err){
+
+        expect(err).toBe(null);
+
+        dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
+
+          expect(err).toBe(null);
+          expect(cursor).toEqual(jasmine.any(String));
+          expect(additions).toEqual(['/test.txt']);
+          expect(deletions).toEqual([]);
+        
+          checkSync(dropboxFolder, dataFolder, function(err){
+
+            expect(err).toBe(null);
+            done();
+          });
+        });
+      });
+    });
+    
+    it("syncs with a cursor", function(done){
+
+      dropbox.writeFile(join(dropboxFolder, 'test.txt'), 'No', function(err){
+
+        expect(err).toBe(null);
+
+        dropbox.sync(dropboxFolder, dataFolder, function(err, cursor, additions, deletions){
+
+          expect(err).toBe(null);
+          expect(cursor).toEqual(jasmine.any(String));
+          expect(additions).toEqual(['/test.txt']);
+          expect(deletions).toEqual([]);
+
+          dropbox.remove(join(dropboxFolder, 'test.txt'), function(err){
+
+            expect(err).toBe(null);
+
+            dropbox.writeFile(join(dropboxFolder, 'another.txt'), 'No', function(err){
+
+              expect(err).toBe(null);
+        
+              dropbox.sync(dropboxFolder, dataFolder, {cursor: cursor}, function(err, cursor, additions, deletions){
+
+                expect(err).toBe(null);
+                expect(cursor).toEqual(jasmine.any(String));
+                expect(additions).toEqual(['/another.txt']);
+                expect(deletions).toEqual(['/test.txt']);
+
+                checkSync(dropboxFolder, dataFolder, function(err){
+
+                  expect(err).toBe(null);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+
+    it("will not redownload a file which matches", function(done){
+
+      dropbox = lib(accessToken);
+
+      dropbox.writeFile(join(dropboxFolder, 'rabbit.txt'), 'hare', function(err){
+
+        expect(err).toBe(null);
+
+        dropbox.sync(dropboxFolder, dataFolder, function(err, firstCursor, additions, deletions){
+
+          expect(err).toBe(null);
+          expect(firstCursor).toEqual(jasmine.any(String));
+          expect(additions).toEqual(['/rabbit.txt']);
+          expect(deletions).toEqual([]);
+        
+          checkSync(dropboxFolder, dataFolder, function(err){
+
+            expect(err).toBe(null);
+
+            dropbox.sync(dropboxFolder, dataFolder, function(err, secondCursor, additions, deletions){
+
+              expect(err).toBe(null);
+              expect(secondCursor).toEqual(jasmine.any(String));
+              expect(firstCursor).not.toEqual(secondCursor);
+              expect(additions).toEqual([]);
+              expect(deletions).toEqual([]);
+                            
+              checkSync(dropboxFolder, dataFolder, function(err){
+
+                expect(err).toBe(null);
+
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  };}
+
+  
 
 });
