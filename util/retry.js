b3bdd5afacb3c006fd583d08716449b05149b2f0
David Merfield <dmerfield@gmail.com>
2018-06-12 17:52:42 -0400

    Handle ENOTFOUND err

--- util/retry.js
+++ util/retry.js
@@ -1,13 +1,63 @@
 var debug = require('debug')('dropbox-extra:retry');
 
+
+/*
+
+
+    // Network error, new sync after a bit
+    if (code === 0 || code === 500 || code === 504) {
+      email.NETWORK_ERROR(uid);
+      log('Network error accessing Dropbox API: ' + code);
+      tryAgain = true;
+    }
+
+    // Hitting rate limits, try again after delay
+    // specified in error.Retry-After ?
+    if (code === 429 || code === 503) {
+      email.RATE_LIMIT(uid);
+      log('Rate limit error accessing Dropbox API: ' + code);
+      tryAgain = true;
+    }
+
+    // User is over their dropbox storage quota
+    if (code === 507) {
+      email.NO_SPACE(uid);
+      log('No space in Dropbox folder: ' + code);
+    }
+
+    // Blot is sending bad requests
+    if (code === 400 || code === 403) {
+      email.BAD_REQUEST(uid);
+      log('Bad request to Dropbox API: ' + code);
+    }
+
+    // The user's dropbox access token has
+    // expired or been revoked. This can be
+    // fixed if the user visits /auth
+    if (code === 401) {
+      email.REVOKED(uid);
+      log('Dropbox access token is invalid: ' + code);
+    }
+
+
+ { [FetchError: request to https://content.dropboxapi.com/2/files/download failed, reason: getaddrinfo ENOTFOUND content.dropboxapi.com content.dropboxapi.com:443]
+  name: 'FetchError',
+  message: 'request to https://content.dropboxapi.com/2/files/download failed, reason: getaddrinfo ENOTFOUND content.dropboxapi.com content.dropboxapi.com:443',
+  type: 'system',
+  errno: 'ENOTFOUND',
+  code: 'ENOTFOUND' }
+
+*/
+
 // HTTP error codes
-var RETRY_CODES = [
+var RETRY_STATUSES = [
   
   0, 500, 504, // network error
-
-  443, // ENOTFOUND?
-
   429, 503 // rate limit error
+]; 
+
+var RETRY_CODES = [
+  'ENOTFOUND'
 ]; 
 
 // 100-200ms between requests
@@ -61,17 +111,10 @@ module.exports = function retry (callback, main, handleErr, options) {
     }
     
     // We know this err is retryable immediately
-    if (RETRY_CODES.indexOf(err.status) !== -1) {
+    if (RETRY_STATUSES.indexOf(err.status) !== -1 ||
+        RETRY_CODES.indexOf(err.code) !== -1) {
 
       debug(err.status + 'is retryable');
-
-      // I want to target the ENOTFOUND error more
-      // specifically than it is now.
-      if (err.status === 443) {
-        console.log('443 ENOTFOUND ERROR?');
-        console.log(err);
-      }
-
       return retryMain();
     }
     
